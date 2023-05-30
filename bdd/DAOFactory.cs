a9a4a512b7650391bff5be3ec464015d3f756bc3
Ahmed KHELOUF <ahmed.khelouf@clsi.fr>
2023-05-30 17:55:23 +0200

    changement nom bdd

--- bdd/DAOFactory.cs
+++ bdd/DAOFactory.cs
@@ -20,7 +20,7 @@ namespace Mediateq_AP_SIO2
             string serverIp = "127.0.0.1";
             string username = "root";
             string password = "";
-            string databaseName = "mediateq2";
+            string databaseName = "mediateq-c";
              
 
             string dbConnectionString = string.Format("server={0};uid={1};pwd={2};database={3}; ", serverIp, username, password, databaseName);
